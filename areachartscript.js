81dce232322531ecfb37d3d1a65d72265ec41df7
eszypulski <111613262+eszypulski@users.noreply.github.com>
2024-05-21 23:43:45 -0400

    incorporated graphs and added styles
    hooray!

--- areachartscript.js
+++ areachartscript.js
@@ -1,18 +1,13 @@
 // SET DIMENSIONS AND MARGINS FOR THE CHART
 const margin = { top: 20, right: 60, bottom: 30, left: 50 };
 const width = 560 - margin.left - margin.right;
-const height = 600 - margin.top - margin.bottom;
+const height =500 - margin.top - margin.bottom;
 
 // SET THE X AND Y SCALES
-
-const x = d3.scaleTime()
-    .range([0, width - 10]);
-
-const y = d3.scaleLinear()
-    .range([height - 10, 0]);
+const x = d3.scaleTime().range([0, width - 10]);
+const y = d3.scaleLinear().range([height - 10, 0]);
 
 // CREATE THE SVG ELEMENT AND APPEND IT TO THE CHART CONTAINER
-
 const svg = d3.select("#areachart")
     .append("svg")
     .attr("width", width + margin.left + margin.right)
@@ -21,18 +16,17 @@ const svg = d3.select("#areachart")
     .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 // LOAD THE DATA
-
 d3.csv("datasets/ccdata.csv").then(data => {
 
     // PARSE THE DATA AND CONVERT THE STRINGS TO NUMBERS
     const parseDate = d3.timeParse("%Y");
     let filteredData;
-    let buttonData;
+    let buttonData = 'mcinfant'; // Initial data source
 
     data.forEach(d => {
         d.date = parseDate(d.date);
-        d.mhi = +d.mhi
-        d.mcinfant = +d.mcinfant
+        d.mhi = +d.mhi;
+        d.mcinfant = +d.mcinfant;
         d.benchmark = +d.benchmark;
         d.county = d.county;
     });
@@ -45,97 +39,103 @@ d3.csv("datasets/ccdata.csv").then(data => {
         .append("option")
         .text(function (d) { return d; });
 
-     // SET UP THE LINE GENERATOR
-const line = d3.line()
-.x(d => x(d.date));
+    // SET UP THE LINE GENERATOR
+    const line = d3.line().x(d => x(d.date));
 
-// SET UP THE AREA GENERATOR
-const area = d3.area()
-.x(d => x(d.date))
-.y0(height - 10);
+    // SET UP THE AREA GENERATOR
+    const area = d3.area().x(d => x(d.date)).y0(height - 10);
 
-// ADD THE X-AXIS
-svg.append("g")
-.attr("class", "x-axis")
-.attr("transform", `translate(0, ${height})`);
+    // ADD THE X-AXIS
+    svg.append("g")
+        .attr("class", "x-axis")
+        .attr("transform", `translate(0, ${height})`);
 
-// ADD THE Y-AXIS
-svg.append("g")
-.attr("class", "y-axis")
-.attr("transform", `translate(${width}, 0)`);
+    // ADD THE Y-AXIS
+    svg.append("g")
+        .attr("class", "y-axis")
+        .attr("transform", `translate(${width}, 0)`);
 
-// ADD THE AREA PATH TO THE CHART
-svg.append("path")
-.attr("class", "area mhi")
-.style("fill", "#85bb65") // CHANGE THE COLOR LATER
-.style("opacity", .5); // CHANGE THE OPACITY LATER
+    // ADD THE AREA PATH TO THE CHART
+    svg.append("path")
+        .attr("class", "area mhi")
+        .style("fill", "#DADADA") // CHANGE THE COLOR LATER
+        .style("opacity", .9); // CHANGE THE OPACITY LATER
 
-// ADD THE LINE PATH TO THE CHART
-svg.append("path")
-.attr("class", "line mhi")
-.style("fill", "none")
-.style("stroke", "#85bb65") // CHANGE THE COLOR LATER
-.style("stroke-width", 2); // CHANGE THE WIDTH LATER
+    // ADD THE LINE PATH TO THE CHART
+    svg.append("path")
+        .attr("class", "line mhi")
+        .style("fill", "none")
+        .style("stroke", "#DADADA") // CHANGE THE COLOR LATER
+        .style("stroke-width", 2); // CHANGE THE WIDTH LATER
 
-// SET UP THE INFANT CARE LINE GENERATOR
-const lineInfant = d3.line()
-.x(d => x(d.date));
+    // SET UP THE INFANT CARE LINE GENERATOR
+    const lineInfant = d3.line().x(d => x(d.date));
 
-// SET UP THE AREA GENERATOR
-const areaInfant = d3.area()
-.x(d => x(d.date))
-.y0(height - 10);
+    // SET UP THE AREA GENERATOR
+    const areaInfant = d3.area().x(d => x(d.date)).y0(height - 10);
 
-// ADD THE AREA PATH TO THE CHART
-svg.append("path")
-.attr("class", "area mcinfant")
-.style("fill", "orange") // CHANGE THE COLOR LATER
-.style("opacity", .5); // CHANGE THE OPACITY LATER
+    // ADD THE AREA PATH TO THE CHART
+    svg.append("path")
+        .attr("class", "area mcinfant")
+        .style("fill", "#21897E") // CHANGE THE COLOR LATER
+        .style("opacity", .9); // CHANGE THE OPACITY LATER
 
-// ADD THE LINE PATH TO THE CHART
-svg.append("path")
-.attr("class", "line mcinfant")
-.style("fill", "none")
-.style("stroke", "orange") // CHANGE THE COLOR LATER
-.style("stroke-width", 2); // CHANGE THE WIDTH LATER 
+    // ADD THE LINE PATH TO THE CHART
+    svg.append("path")
+        .attr("class", "line mcinfant")
+        .style("fill", "none")
+        .style("stroke", "#21897E") // CHANGE THE COLOR LATER
+        .style("stroke-width", 2); // CHANGE THE WIDTH LATER 
 
-// Function to update the chart based on the selected county
-function updateChart() {
-// Get the selected county from the dropdown menu
-let selectedCounty = d3.select("#countyDropdown").property("value");
+    // Function to update the chart based on the selected county and data source
+    function updateChart() {
+        // Get the selected county from the dropdown menu
+        let selectedCounty = d3.select("#countyDropdown").property("value");
 
-// Filter the data to include only data from the selected county
-filteredData = data.filter(function (d) { return d.county === selectedCounty; });
+        // Filter the data to include only data from the selected county
+        filteredData = data.filter(function (d) { return d.county === selectedCounty; });
 
-// SET THE X AND Y DOMAINS
-x.domain(d3.extent(filteredData, d => d.date));
-y.domain([0, (d3.max(data, d => d.mhi)) + 2000]);
+        // SET THE X AND Y DOMAINS
+        x.domain(d3.extent(filteredData, d => d.date));
+        y.domain([0, (d3.max(data, d => d.mhi)) + 2000]);
 
-// Update the axes
-svg.select(".x-axis").call(d3.axisBottom(x));
-svg.select(".y-axis").call(d3.axisRight(y).ticks(12));
+        // Update the axes
+        svg.select(".x-axis").call(d3.axisBottom(x));
+        svg.select(".y-axis").call(d3.axisRight(y).ticks(12));
 
-// Update the area and line paths
-svg.select(".area.mhi")
-    .datum(filteredData)
-    .attr("d", area.y1(d => y(d.mhi)));
+        // Update the area and line paths
+        svg.select(".area.mhi")
+            .datum(filteredData)
+            .attr("d", area.y1(d => y(d.mhi)));
 
-svg.select(".line.mhi")
-    .datum(filteredData)
-    .attr("d", line.y(d => y(d.mhi)));
+        svg.select(".line.mhi")
+            .datum(filteredData)
+            .attr("d", line.y(d => y(d.mhi)));
 
-svg.select(".area.mcinfant")
-    .datum(filteredData)
-    .attr("d", areaInfant.y1(d => y(d.mcinfant)));
+        // Use the selected data source for the infant care line and area
+        svg.select(".area.mcinfant")
+            .datum(filteredData)
+            .attr("d", areaInfant.y1(d => y(d[buttonData])));
 
-svg.select(".line.mcinfant")
-    .datum(filteredData)
-    .attr("d", lineInfant.y(d => y(d.mcinfant)));
-}
+        svg.select(".line.mcinfant")
+            .datum(filteredData)
+            .attr("d", lineInfant.y(d => y(d[buttonData])));
+    }
 
-// Call the updateChart function initially
-updateChart();
+    // Call the updateChart function
+    updateChart();
 
-// Add an event listener to the dropdown menu to update the chart when the selection changes
-d3.select("#countyDropdown").on("change", updateChart); })
+    // Add an event listener to the dropdown menu to update the chart when the selection changes
+    d3.select("#countyDropdown").on("change", updateChart);
 
+    // Add event listeners to the buttons to toggle the data source and update the chart
+    d3.select("#buttonBenchmark").on("click", function () {
+        buttonData = 'benchmark';
+        updateChart();
+    });
+
+    d3.select("#buttonActual").on("click", function () {
+        buttonData = 'mcinfant';
+        updateChart();
+    });
+});
--- bar.js
+++ bar.js
@@ -1,215 +1,128 @@
-// code inspired by the UNHCR's DataViz site: https://dataviz.unhcr.org/tools/d3/d3_stacked_column_100perc_chart.html
-
-
 // set the dimensions and margins of the graph
-const margin = {top: 80, right: 20, bottom: 50, left: 40};
-const width = 300 - margin.left - margin.right;
-const height = 350 - margin.top - margin.bottom;
+const marginBar = { top: 5, right: 35, bottom: 50, left: 80 };
+const widthBar = 450 - marginBar.left - marginBar.right;
+const heightBar = 350 - marginBar.top - marginBar.bottom;
 
 // append the svg object to the body of the page
-const svg = d3.select("#viz_container")
-  .append("svg")
+const svgBar = d3.select("#viz_container")
+    .append("svg")
     .attr("width", "100%")
     .attr("height", "100%")
     .attr("viewBox", "0 0 450 350")
     .attr("preserveAspectRatio", "xMinYMin")
-  .append("g")
-    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+    .append("g")
+    .attr("transform", `translate(${marginBar.left}, ${marginBar.top})`);
 
 // parse the Data - REPLACED WITH MY OWN
 d3.csv("datasets/2018data.csv")
-.then(function(data){
+    .then(function (dataBar) {
 
-// list of value keys
-const typeKeys = data.columns.slice(1);
+        // list of value keys
+        const typeKeys = dataBar.columns.slice(1);
 
-// stack the data
-const stack = d3.stack()
-   .keys(typeKeys)
-   .order(d3.stackOrderNone)
-   .offset(d3.stackOffsetNone)
-const stackedData =stack(data)
+        // stack the data
+        const stack = d3.stack()
+            .keys(typeKeys)
+            .order(d3.stackOrderNone)
+            .offset(d3.stackOffsetNone)
+        const stackedData = stack(dataBar)
 
-/*// X scale and Axis
-const xScale = d3.scaleLinear()
-    .domain([0, 1])
-    .range([0, width]);
+        // X scale and Axis
+        const xScaleBar = d3.scaleLinear()
+            .domain([0, 1])
+            .range([0, widthBar]);
 
-svg
-    .append('g')
-    .attr("transform", `translate(0, ${height})`)
-    .call(d3.axisBottom(xScale).ticks(10, "%").tickSize(0).tickPadding(8));
+        svgBar
+            .append('g')
+            .attr("transform", `translate(0, ${heightBar})`)
+            .call(d3.axisBottom(xScaleBar).ticks(10, "%").tickSize(0).tickPadding(8));
 
-// Y scale and Axis
-const yScale = d3.scaleBand()
-    .domain(data.map(d => d.date))
-    .range([0, height])
-    .padding(.2);
+        // Y scale and Axis
+        const yScaleBar = d3.scaleBand()
+            .domain(dataBar.map(d => d.date))
+            .range([0, heightBar])
+            .padding(.2);
 
-svg
-    .append('g')
-    .call(d3.axisLeft(yScale).tickSize(0).tickPadding(4))
-    .call(d => d.select(".domain").remove()); */
+        svgBar
+            .append('g')
+            .call(d3.axisLeft(yScaleBar).tickSize(0).tickPadding(4))
+            .call(d => d.select(".domain").remove());
 
-// X scale and Axis
-const xScale = d3.scaleBand()
-  .domain(data.map(d => d.date))
-  .range([0, width])
-  .padding(.2);
-svg
-  .append('g')
-  .attr("transform", `translate(0, ${height})`)
-  .call(d3.axisBottom(xScale).tickSize(0).tickPadding(8));
+        // color palette
+        const colorBar = d3.scaleOrdinal()
+            .domain(typeKeys)
+            .range(["#21897E", "#DADADA", "#7EBCE6"])
+        
 
-// Y scale and Axis
-const yScale = d3.scaleLinear()
-    .domain([0, 1])
-    .range([height, 0]);
-svg
-  .append('g')
-  .call(d3.axisLeft(yScale).ticks(10, "%").tickSize(0).tickPadding(4))
-  .call(d => d.select(".domain").remove());
+        // create bars
+        svgBar.append("g")
+            .selectAll("g")
+            .data(stackedData)
+            .join("g")
+            .attr("fill", d => colorBar(d.key))
+            .selectAll("rect")
+            .data(d => d)
+            .join("rect")
+            .attr("y", d => yScaleBar(d.data.date))
+            .attr("x", d => xScaleBar(0))
+            .attr("height", yScaleBar.bandwidth())
+            .attr("width", d => xScaleBar(d[1]) - xScaleBar(d[0]))
+            .attr("x", d => xScaleBar(d[0]))
 
-// color palette
-const color = d3.scaleOrdinal()
-  .domain(typeKeys)
-  .range(["#18375F", "#0072BC", "#8EBEFF", "#00B398"])
+        
 
-// set horizontal grid line
-const GridLine = function() {return d3.axisLeft().scale(yScale)};
-svg
-  .append("g")
-    .attr("class", "grid")
-  .call(GridLine()
-    .tickSize(-width,0,0)
-    .tickFormat("")
-);
-
-// create a tooltip
-const tooltip = d3.select("body")
-  .append("div")
-    .attr("class", "tooltip");
-
-// tooltip events
-const mouseover = function(d) {
-    tooltip
-        .style("opacity", 1)
-    d3.select(this)
-        .style("stroke", "#EF4A60")
-        .style("opacity", .5)
-};
-const mousemove = function(event,d) {
-    const subgroupName = d3.select(this.parentNode).datum().key;
-    const subgroupValue = d.data[subgroupName];
-    const f = d3.format(".0f");
-    tooltip
-    .html(`<b>${subgroupName}</b>:  ${f(subgroupValue*100)}%`)
-        .style("top", event.pageY - 10 + "px")
-        .style("left", event.pageX + 10 + "px")
-};
-const mouseleave = function(d) {
-    tooltip
-        .style("opacity", 0)
-    d3.select(this)
-        .style("stroke", "none")
-        .style("opacity", 1)
-};
-
-// create bars
-svg.append("g")
-  .selectAll("g")
-  .data(stackedData)
-  .join("g")
-      .attr("fill", d => color(d.key))
-  .selectAll("rect")
-  .data(d => d)
-  .join("rect")
-      .attr("x", d => xScale(d.data.date))
-      .attr("y", d => yScale(d[1]))
-      .attr("width", xScale.bandwidth())
-      .attr("height", d => yScale(d[0]) - yScale(d[1]))
-  .on("mouseover", mouseover)
-  .on("mousemove", mousemove)
-  .on("mouseleave", mouseleave)
-
-// set title
-svg
-  .append("text")
-    .attr("class", "chart-title")
-    .attr("x", -(margin.left)*0.7)
-    .attr("y", -(margin.top)/1.5)
-    .attr("text-anchor", "start")
-  .text("Levels of earmarking | 2012-2020")
-
-// set source
-svg
-  .append("text")
-    .attr("class", "chart-source")
-    .attr("x", -(margin.left)*0.7)
-    .attr("y", height + margin.bottom*0.7)
-    .attr("text-anchor", "start")
-  .text("Source: UNHCR")
-
-// set copyright
-svg
-  .append("text")
-    .attr("class", "copyright")
-    .attr("x", -(margin.left)*0.7)
-    .attr("y", height + margin.bottom*0.9)
-    .attr("text-anchor", "start")
-  .text("©UNHCR, The UN Refugee Agency")
-
-//set legend
-svg
-    .append("rect")
-        .attr("x", -(margin.left)*0.7)
-        .attr("y", -(margin.top/2.5))
-        .attr("width", 13)
-        .attr("height", 13)
-        .style("fill", "#18375F")
-svg
-    .append("text")
-        .attr("class", "legend")
-        .attr("x", -(margin.left)*0.6+15)
-        .attr("y", -(margin.top/3.5))
-    .text("Earmarked")
-svg
-    .append("rect")
-        .attr("x", 60)
-        .attr("y", -(margin.top/2.5))
-        .attr("width", 13)
-        .attr("height", 13)
-        .style("fill", "#0072BC")
-svg
-    .append("text")
-        .attr("class", "legend")
-        .attr("x", 80)
-        .attr("y", -(margin.top/3.5))
-    .text("Softly earmarked")
-svg
-    .append("rect")
-        .attr("x", 170)
-        .attr("y", -(margin.top/2.5))
-        .attr("width", 13)
-        .attr("height", 13)
-        .style("fill", "#8EBEFF")
-svg
-    .append("text")
-        .attr("class", "legend")
-        .attr("x", 190)
-        .attr("y", -(margin.top/3.5))
-    .text("Tightly earmarked")
-svg
-    .append("rect")
-        .attr("x", 290)
-        .attr("y", -(margin.top/2.5))
-        .attr("width", 13)
-        .attr("height", 13)
-        .style("fill", "#00B398")
-svg
-    .append("text")
-        .attr("class", "legend")
-        .attr("x", 310)
-        .attr("y", -(margin.top/3.5))
-    .text("Unearmarked")
-})
+ 
+        /*set legend
+        svgBar
+            .append("rect")
+            .attr("x", 5)
+            .attr("y",5)
+            .attr("width", 13)
+            .attr("height", 13)
+            .style("fill", "#18375F")
+        svgBar
+            .append("text")
+            .attr("class", "legend")
+            .attr("x", 200)
+            .attr("y", 200)
+            .text("Earmarked")
+        svgBar
+            .append("rect")
+            .attr("x", 60)
+            .attr("y", -(marginBar.top / 2.5))
+            .attr("width", 13)
+            .attr("height", 13)
+            .style("fill", "#0072BC")
+        svgBar
+            .append("text")
+            .attr("class", "legend")
+            .attr("x", 80)
+            .attr("y", -(marginBar.top / 3.5))
+            .text("Softly earmarked")
+        svgBar
+            .append("rect")
+            .attr("x", 170)
+            .attr("y", -(marginBar.top / 2.5))
+            .attr("width", 13)
+            .attr("height", 13)
+            .style("fill", "#8EBEFF")
+        svgBar
+            .append("text")
+            .attr("class", "legend")
+            .attr("x", 190)
+            .attr("y", -(marginBar.top / 3.5))
+            .text("Tightly earmarked")
+        svgBar
+            .append("rect")
+            .attr("x", 290)
+            .attr("y", -(marginBar.top / 2.5))
+            .attr("width", 13)
+            .attr("height", 13)
+            .style("fill", "#00B398")
+        svgBar
+            .append("text")
+            .attr("class", "legend")
+            .attr("x", 310)
+            .attr("y", -(marginBar.top / 3.5))
+            .text("Unearmarked")*/
+    })
